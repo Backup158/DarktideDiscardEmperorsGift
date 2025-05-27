f6f6df198a2573eced1a36ce101dbf9da1b779f9
LackupL58 <beekoop158+gh@gmail.com>
2025-05-26 23:37:53 -0600

    style: comments

--- DiscardEmperorsGift/scripts/mods/DiscardEmperorsGift/DiscardEmperorsGift.lua
+++ DiscardEmperorsGift/scripts/mods/DiscardEmperorsGift/DiscardEmperorsGift.lua
@@ -18,7 +18,7 @@ local heretical_quotes = {
 
 -- #######
 -- Refresh settings
---  Not for performance. I just want to save myself from typing mod:get all the time
+--  NOT for performance. I just want to save myself from typing mod:get all the time
 -- #######
 function mod.get_settings()
     mod.using_debug_mode = mod:get("enable_debug_mode")
@@ -57,6 +57,9 @@ local discard_item = function(id_for_item_to_discard)
     local discard_item_promise = Managers.data_service.gear_service:delete_gear(id_for_item_to_discard)
 end
 
+-- #########################################
+-- On Load and Setting Changed
+-- #########################################
 function mod.on_all_mods_loaded()
     mod:info("DiscardEmperorsGift v"..mod_version.." loaded uwu nya :3")
     mod.get_settings()
